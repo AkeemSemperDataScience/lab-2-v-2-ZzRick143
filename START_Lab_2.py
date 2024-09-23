615ed890884eed9c6d2bee6bfebeeed034f6edf3
ZzRick143 <ChenRichard1991@gmail.com>
2024-09-23 02:34:18 -0600

    test5

--- START_Lab_2.py
+++ START_Lab_2.py
@@ -29,7 +29,9 @@ def lab2Question3(str1, str2):
     # Create a function that takes in two strings - str1 and str2
     # Return the number of times str2 appears in str1
     # For example if str1 = "coding is cool" and str2 = "co" then output should be 2.
-    pass
+    substring = str2
+    count = str1.count(substring)
+    return count
 
 def lab2Question4(list1, list2):
     # Create a function that takes in two equal length list of numbers. 
